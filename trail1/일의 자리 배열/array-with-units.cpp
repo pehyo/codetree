64e2f2c5760c26a2dc8861e608d76dc9f6f1532c
pehyo <pehyo153810@gmail.com>
2026-07-12 22:19:52 +0900

    [Passed] 일의 자리 배열 (2ms, 0MB)

---
+++ trail1/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1=0,n2=0;
+    int arr[10]={};
+    cin>>n1>>n2;
+    arr[0]=n1,arr[1]=n2;
+    for(int i=2;i<10;++i){
+        arr[i]=(arr[i-1]+arr[i-2])%10;
+    }
+
+    for(int i=0;i<10;++i)
+        cout<<arr[i]<<" ";
+
+    return 0;
+}
