d0e1c19255d0e40f1dd2630ddf347891c44c9a7a
pehyo <pehyo153810@gmail.com>
2026-07-20 23:32:17 +0900

    [Passed] 정수 직사각형 (2ms, 0MB)

---
+++ trail1/정수 직사각형/rectangle-of-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    int arr[100][100]={};
+    int num=1;    
+    cin>>n>>m;
+    
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            arr[i][j]=num;
+            ++num;
+        }
+    }
+
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            cout<<arr[i][j]<<" ";
+        }
+    cout<<endl;
+    }
+    return 0;
+}
