798c29daebe1ceb5de3aeeeb531956f1018e23f0
pehyo <pehyo153810@gmail.com>
2026-07-31 18:36:48 +0900

    [Passed] 두 문자열을 이어붙였을 때 (2ms, 0MB)

--- trail1/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
+++ trail1/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -9,7 +9,7 @@ int main() {
     string A=a+b;
     string B=b+a;
     int len = A.length();
-    string same ="false";
+    string same ="false"; //if문 조건으로 A==B로 한번에 처리 가능함!! 주의하기!!!!
     for(int i=0;i<len;++i){
         if(A[i]!=B[i])
             break;
