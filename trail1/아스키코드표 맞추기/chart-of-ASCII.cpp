62917b2e999b05cbcc9ecf40d050ce4ef85069e6
pehyo <pehyo153810@gmail.com>
2026-08-05 14:03:24 +0900

    [Passed] 아스키코드표 맞추기 (2ms, 0MB)

---
+++ trail1/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    
+    cout<<(char)a<<" "<<(char)b<<" "<<(char)c<<" "<<(char)d<<" "<<(char)e<<" ";
+    return 0;
+}
